df15e5d67089d6051b77afd4c8ae7db3e78cc0a5
Val <thimxx@gmail.com>
2017-08-07 22:59:59 +0200

    Ok... my mistake. Not geni_settings is in

---
+++ pyGeni/geni_settings.py
@@ -0,0 +1,6 @@
+#Several addresses of the Geni API
+GENI_ADDRESS = "https://www.geni.com"
+GENI_VALIDATE_TOKEN = GENI_ADDRESS + "/platform/oauth/validate_token?access_token="
+GENI_PROFILE = GENI_ADDRESS + "/api/profile-"
+GENI_FAMILY = "/immediate-family"
+GENI_TOKEN = "?access_token="
