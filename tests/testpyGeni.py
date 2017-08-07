50a93101b8dbb28619390800199e2edc9ce7feb7
Val <thimxx@gmail.com>
2017-08-08 01:53:30 +0200

    Improved coverage

--- tests/testpyGeni.py
+++ tests/testpyGeni.py
@@ -16,6 +16,14 @@ class testpyGeni(unittest.TestCase):
         assert( "Felipe" in self.philip.nameLifespan())
         assert( "1605" in self.philip.nameLifespan())
         assert( "1665" in self.philip.nameLifespan())
+        
+    def testUsingIdProfile(self):
+        '''
+        This test checks that using a different input id the id obtained is the same.
+        '''
+        token = os.environ['GENI_KEY']
+        philip_bis = profile.profile(PHILIPIVget, token , "")
+        assert( philip_bis.get_id() == self.philip.get_id())
 
     def testRelationsInfo(self):
         '''
