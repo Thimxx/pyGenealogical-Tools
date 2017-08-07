6df3275b1d9c9bc0a2b1ed0bc3b3652c5f9adeb8
Val <thimxx@gmail.com>
2017-08-07 23:54:23 +0200

    First creation of files for ancestor climn

---
+++ analyzefamily/ancerstors_climb.py
@@ -0,0 +1,17 @@
+'''
+Created on 7 ago. 2017
+
+@author: Val
+'''
+
+class climb(object):
+    '''
+    This class will provide a list of ancestors growing up from the current profile
+    '''
+
+
+    def __init__(self, source_person):
+        '''
+        As input it is needed a class pyGeni.profile already obtained.
+        '''
+        
