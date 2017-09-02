0ae7acac252a9a164a4708502d4e30c6a93c4cb2
Val <thimxx@gmail.com>
2017-09-03 01:33:29 +0200

    Updated travis configuration of icons

--- pyGeni/__init__.py
+++ pyGeni/__init__.py
@@ -25,17 +25,16 @@ def update_geni_address(new_geni_address):
     This function will update the values of geni variables to be able to use
     in testing mode
     '''
-    global GENI_ADDRESS 
-    global GENI_VALIDATE_TOKEN 
-    global GENI_API 
+    global GENI_ADDRESS
+    global GENI_VALIDATE_TOKEN
+    global GENI_API
     global GENI_PROFILE
     global GENI_SINGLE_TOKEN
-    
+    #We update the variables
     GENI_ADDRESS = new_geni_address
     GENI_VALIDATE_TOKEN = GENI_ADDRESS + "/platform/oauth/validate_token" + GENI_SINGLE_TOKEN
     GENI_API = GENI_ADDRESS + "/api/"
     GENI_PROFILE =  GENI_API + "profile-"
-    
 def geni_request_get(url):
     '''
     Function to perform get calls.
